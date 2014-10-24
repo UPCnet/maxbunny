02af97e39f959f1015a45428f90f83699dd309e6
Carles Bruguera <carlesba@gmail.com>
2014-10-24 08:51:07 +0200

    Pin apns-client

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
 requires = [
     'maxcarrot',
     'maxclient [wsgi]',
-    'apns_client',
+    'apns_client==0.1.8',
     'gcm-client',
     'rabbitpy',
     'gevent'
