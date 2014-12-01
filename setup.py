2f442ee3d1f619b84f7cdbd80d3dac1358b2f276
Carles Bruguera <carlesba@gmail.com>
2014-12-01 13:11:03 +0100

    Back to development: 4.0.15

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
 ]
 
 setup(name='maxbunny',
-      version='4.0.14',
+      version='4.0.15.dev0',
       description='Consumer of AMPQ queues for MAX',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
