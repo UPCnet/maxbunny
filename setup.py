87017bc67ce1fb93f8761e287dfb2d4a5b9ccd60
Pilar Marinas <pilar.marinas@upcnet.es>
2024-10-08 14:13:11 +0200

    Back to development: 5.14

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ requires = [
 ]
 
 setup(name='maxbunny',
-      version='5.13',
+      version='5.14.dev0',
       description='Consumer of AMPQ queues for MAX',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
